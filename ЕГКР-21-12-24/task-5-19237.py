53da140a622d6956821f677da87d79a6ebd05af4
GPetrovskiy <clxwnhopelesss@yandex.ru>
2024-12-24 17:01:02 +0200

    Решил 5 задание

--- ЕГКР-21-12-24/task-5-19237.py
+++ ЕГКР-21-12-24/task-5-19237.py
@@ -1,4 +1,18 @@
-print('Hello')
-
-
-print('World')
+def convert(num, sys):
+    r = ''
+    while num:
+        r += str(num%sys)
+        num //= sys
+    return r[::-1]
+res = []
+for N in range(1, 10_000):
+    s = convert(N, 3)
+    if N % 3 == 0:
+        s += s[-2:]
+    else:
+        sumi = sum(map(int, s))
+        s += convert(sumi, 3)
+    R = int(s, 3)
+    if R % 2 == 0 and R > 220:
+        res.append(R)
+print(min(res))
