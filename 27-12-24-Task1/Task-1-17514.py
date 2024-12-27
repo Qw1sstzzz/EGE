27c921272e9866157da1fd94b5a6d41858e8cd4c
GPetrovskiy <clxwnhopelesss@yandex.ru>
2024-12-27 17:29:39 +0200

    Решено задание №1 (17514)

---
+++ 27-12-24-Task1/Task-1-17514.py
@@ -0,0 +1,13 @@
+from itertools import *
+
+graph = 'AB BH AH HF FD DC FG GC GE CE EA'.split()
+matrix = '247 148 578 126 38 47 136 235'.split()
+
+print(*range(1, 9))
+
+for i in permutations('ABCDEFGH'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
+        break
+print('')
+print(30+8)
