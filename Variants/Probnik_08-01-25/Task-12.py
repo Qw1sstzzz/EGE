275aba57d96bc641669aef6caebf20060b4da38e
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-08 11:40:39 +0200

    9 (12-ое задание)

---
+++ Variants/Probnik_08-01-25/Task-12.py
@@ -0,0 +1,12 @@
+ans = []
+for n in range(4, 1000):
+    s = '1' + n * '2'
+    while '12' in s or '322' in s or '222' in s:
+        s = s.replace('12', '2', 1)
+        s = s.replace('322', '21', 1)
+        s = s.replace('222', '3', 1)
+    print(s)
+    ans.append(len(s))
+print(max(ans))
+
+# 9
