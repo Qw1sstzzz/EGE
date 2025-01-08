0dfe60d809dc5acd2fda6efd2965170025c12d54
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-08 14:20:06 +0200

    Решал-решал, и так к ответу не пришёл правильному.Дома доразобрать(((

--- Variants/Probnik_08-01-25/Task-24.py
+++ Variants/Probnik_08-01-25/Task-24.py
@@ -5,20 +5,8 @@ A_first = data.find('A')
 A_last = data.rfind('A')
 data = data[A_first:A_last+1]
 
-t = []
-for i in range(len(data) - 2):
-    ps = data[i]
-    for j in range(i+1, len(data) - 1):
-        ps += data[j]
-        if ps[-1] == 'A':
-            break
-    t.append(ps)
-    ps = ''
-res = []
-for l in t:
-    if l.count('A') != 1 and len(l) != l.count('A'):
-        res.append(l)
-print(len(res))
-
-
-
+pos_a = [i for i in range(len(data)) if data[i] == 'A']
+cnt = 0
+for i in range(len(pos_a) - 1):
+    if pos_a[i+1] - pos_a[i] > 1:
+        cnt += 1
