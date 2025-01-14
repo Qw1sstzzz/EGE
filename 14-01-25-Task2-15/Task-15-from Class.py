f5db73d17583c3f0a95286ad29534328a17f5c8f
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-14 17:58:23 +0200

    16898 (Исправил ошибку с rang'ами)

--- 14-01-25-Task2-15/Task-15-from Class.py
+++ 14-01-25-Task2-15/Task-15-from Class.py
@@ -1,7 +1,7 @@
 def f(x, A):
     return (x & 21074 != 0) <= ((x & 12369 == 0) <= (x & A != 0))
 
-for A in range(0, 10_000):
-    if all(f(x, A) for x in range(0, 10_000)):
+for A in range(0, 30000):
+    if all(f(x, A) for x in range(0, 30000)):
         print(A)
         break
