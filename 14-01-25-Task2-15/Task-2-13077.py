9cb76885ae48fd799cb525c2ad35f91151a40c99
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-14 17:55:34 +0200

    zywx (2 функции)

---
+++ 14-01-25-Task2-15/Task-2-13077.py
@@ -0,0 +1,20 @@
+from itertools import product, permutations
+
+def f1(x, y, w, z):
+    return (w == x) and (y <= z)
+def f2(x, y, w, z):
+    return (w <= x) <= (y == z)
+
+for a1, a2, a3, a4, a5 in product([1, 0], repeat=5):
+    table = [(1, a1, 1, 1, 1, 0),
+             (a2, 1, 0, 0, 1, a3),
+             (a4, 0, 0, a5, 0, 0)]
+    if len(set(table)) == len(table):
+        for p in permutations('xywz'):
+            u1 = [f1(**dict(zip(p, t))) for t in table]
+            u2 = [f2(**dict(zip(p, t))) for t in table]
+            if u1 == [1, 1, 0]:
+                if u2 == [0, a3, 0]:
+                    print(*p)
+
+# zywx
