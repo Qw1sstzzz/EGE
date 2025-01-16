448a72b4b4257bc36061ca03fc189312e805b7e8
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-17 00:32:24 +0300

    zyxw

---
+++ Homework/17-01-25/Task-2-17665.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(z, y, x, w):
+    return (z <= (not (y <= x))) or w
+
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [
+        (a1, 1, a2, a3),
+        (a4, a5, 0, 0),
+        (a6, 0, 1, a7)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [0, 0, 0]:
+                print(*p)
+
+# zyxw
