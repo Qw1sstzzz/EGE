6839809ee082c77d7239e4ae21b40f91281b561f
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-17 00:35:02 +0300

    yxwz

---
+++ Homework/17-01-25/Task-2-19234.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(z, y, x, w):
+    return (not (((not x) or y) and (not w))) or (not (z and (not (y and w))))
+
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [
+        (0, a1, a2, 1),
+        (a3, 1, a4, a5),
+        (1, 0, a6, a7)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [0, 0, 0]:
+                print(*p)
+
+# yxwz
