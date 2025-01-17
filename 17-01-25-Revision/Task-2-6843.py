4f8ed026e5e10a7ad080c7fb39a464abbcfbeedb
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-17 17:29:10 +0200

    zwyx

---
+++ 17-01-25-Revision/Task-2-6843.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(y, x, z, w):
+    return (z <= w) and y and (not x)
+
+for a1, a2, a3, a4, a5 in product([0, 1], repeat=5):
+    table = [
+        (0, 1, a1, 0),
+        (a2, 0, a3, a4),
+        (0, 1, 1, a5)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [1, 1, 0]:
+                print(*p)
+
+# zwyx
