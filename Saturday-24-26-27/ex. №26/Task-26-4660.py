d18ec91730e07bff619ed9787641b7efe43f7b7c
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-18 18:17:54 +0300

    (26-4660) 44101521 48825239

--- Saturday-24-26-27/ex. №26/Task-26-4660.py
+++ Saturday-24-26-27/ex. №26/Task-26-4660.py
@@ -2,15 +2,13 @@ with open('Task-26-4660.txt') as file:
     N = int(file.readline().strip())
     data = [int(i) for i in file]
 
-data = sorted(data, key=lambda x: -x)
-copy_data = sorted(data)
+data, copy_data = sorted(data, key=lambda x: -x), sorted(data)
 
 price_4_customer = sum([data[i] for i in range(3, len(data), 4)])
+price_4_shop = sum([i for i in copy_data[:N//4]])
 
-price_4_shop = sum([i for i in data[::-1][:N//4]])
-
-ans1 = sum([int(i) for i in data]) - price_4_customer + price_4_customer // 2
-ans2 = sum([int(i) for i in data]) - price_4_shop + price_4_shop // 2
+ans1 = sum(data) - price_4_customer + price_4_customer // 2
+ans2 = sum(data) - price_4_shop + price_4_shop // 2
 
 print(ans1, ans2, end='\n')
 
