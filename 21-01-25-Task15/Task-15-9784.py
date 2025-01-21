3d34800ce807941dbe8ce2a27befbe697bd84483
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-21 18:33:38 +0300

    82

---
+++ 21-01-25-Task15/Task-15-9784.py
@@ -0,0 +1,7 @@
+def f(x, y, A):
+    return (x * y < A) or (x < y) or (9 < x)
+
+for A in range(0, 1000):
+    if all(f(x, y, A) for x in range(0, 1000) for y in range(0, 1000)):
+        print(A)
+        break
