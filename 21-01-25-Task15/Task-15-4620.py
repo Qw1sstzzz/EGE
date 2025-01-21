ceda60bdc218be349d520acd77a9cba1db27d381
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-21 17:43:34 +0300

    9

---
+++ 21-01-25-Task15/Task-15-4620.py
@@ -0,0 +1,9 @@
+def f(x, y, a):
+    return (x + y <= 22) or (y <= x - 6) or (y >= a)
+
+for a in range(0, 10_000)[::-1]:
+    if all(f(x, y, a) for x in range(1, 1000) for y in range(1, 1000)):
+        print(a)
+        break
+
+# 9
