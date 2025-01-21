08113f30f9028542d1dddcb9e7595e22e624ba7f
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-21 15:12:59 +0300

    yxz

---
+++ Homework/21-01-25/Task-2-816.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(x, y, z):
+    return not (x == (y <= z))
+
+
+table = [
+    (0, 0, 1),
+    (0, 1, 1)
+]
+
+if len(set(table)) == len(table):
+    for p in permutations('xyz'):
+        u = [f(**dict(zip(p, t))) for t in table]
+        if u == [1, 0]:
+            print(*p)
+
+# yxz
