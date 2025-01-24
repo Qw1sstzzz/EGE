430c469bc30ca78dab0a191eec799e6d87b36b31
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-24 16:18:55 +0200

    2014882386 (?)

---
+++ 24-01-25-Task15-Revision/Task-14-19359.py
@@ -0,0 +1,9 @@
+from string import digits, ascii_uppercase
+
+alph = digits + ascii_uppercase
+
+for x in alph[:22]:
+    s1 = int(f'A23{x}AC0', 22)
+    s2 = int(f'GB{x}21670', 22)
+    if (s1 + s2) % 21 == 0:
+        print(x, (s1 + s2) // 21)
