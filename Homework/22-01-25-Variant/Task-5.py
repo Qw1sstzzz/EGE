af29607e31cbefe02eaacacf65446e01768c322f
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-27 00:01:35 +0200

    141

---
+++ Homework/22-01-25-Variant/Task-5.py
@@ -0,0 +1,18 @@
+def convert(num, sys):
+    r = ''
+    while num:
+        r += str(num % sys)
+        num //= sys
+    return r[::-1]
+
+ans = []
+for N in range(1, 10_000):
+    s = convert(N, 3)
+    if N % 3 == 0: s += s[-2:]
+    else: s += convert(N % 3 * 5, 3)
+    R = int(s, 3)
+    if R > 133:
+        ans.append(R)
+print(min(ans))
+
+# 141
