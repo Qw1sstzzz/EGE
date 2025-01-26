411350849c38991d7e8833b6b0e967489a4748e1
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-26 23:49:39 +0200

    zwyx

---
+++ Homework/22-01-25-Variant/Task-2.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(w, y, x, z):
+    return (w and y) or ((x <= w) == (y <= z))
+
+for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
+    table = [
+        (a1, a2, a3, 1),
+        (1, a4, a5, 1),
+        (1, a6, 1, 1)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [0, 0, 0]:
+                print(*p)
+
+# zwyx
