4acc7f80d47e6b8f5d6f701871969742248153e4
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-01-27 18:00:53 +0200

    57

--- Homework/22-01-25-Variant/Task-24.py
+++ Homework/22-01-25-Variant/Task-24.py
@@ -2,6 +2,9 @@ with open('Task-24-file.txt') as file:
     data = file.readline()
 
 data = data.replace('N', '*').replace('O', '*').replace('P', '*')
-data = data.replace('**', '* *')
+while '**' in data:
+    data = data.replace('**', '* *')
 data = data.split()
 print(len(max(data, key=len)))
+
+# 57
