dc4914df1657f2b59300866fdea02b70623d2ffe
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-28 16:19:28 +0200

    фигня с 25😭

--- Homework/22-01-25-Variant/Task-25.py
+++ Homework/22-01-25-Variant/Task-25.py
@@ -2,7 +2,7 @@ from fnmatch import fnmatch
 def divs(num):
     cnt = 0
     arr = []
-    for x in range(2, round(num**0.5) + 1, 2):
+    for x in range(1, round(num**0.5) + 1, 2):
         if num % x == 0:
             cnt += 1
             arr.append(x)
@@ -11,9 +11,13 @@ def divs(num):
     else:
         return [0, arr]
 
+c = 0
 for x in range(65001, 100**100):
     if fnmatch(str(x), '6*97*5?'):
         if divs(x)[0] == 1:
             summi = divs(x)[1]
             summi = sum(summi)
             print(x, summi)
+            c += 1
+        if c == 7:
+            break
