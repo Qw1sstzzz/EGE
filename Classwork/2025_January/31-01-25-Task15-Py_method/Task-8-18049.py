46674071be7686f3007c8467eb39b59c45333e72
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-01-31 16:46:58 +0200

    2624400😭 (ошибка с set и len)

--- Classwork/2025_January/31-01-25-Task15-Py_method/Task-8-18049.py
+++ Classwork/2025_January/31-01-25-Task15-Py_method/Task-8-18049.py
@@ -7,9 +7,9 @@ for val in product(alph, repeat=7):
     s = ''.join(val)
     if s[0] not in '0246':
         r = s[-3:]
-        if len(r) == len(set(r)):
+        if len(set(r)) != 1:
             cnt += 1
 
 print(cnt)
 
-# 1837080
+# 2624400
