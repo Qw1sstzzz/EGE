cee8321a45c8fb0de7cfe1a39b0c1c3e819d6aee
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-02-02 23:18:57 +0200

    22

---
+++ Homework/2025_January/31-01-25/Task-15-12469.py
@@ -0,0 +1,11 @@
+f_usl = 1
+A = 0
+for x in [k*0.25 for k in range(-10_000, 10_000)]:
+    D = 7 <= x <= 68
+    C = 29 <= x <= 100
+    f = D <= (((not C) and (not A)) <= (not D))
+    if f != f_usl:
+        print(x)
+
+print(29 - 7)
+# 22
