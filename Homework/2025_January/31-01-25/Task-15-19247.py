c454351bea6663311fa2177d893fde4253b8a03d
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-02-02 23:13:42 +0200

    54

---
+++ Homework/2025_January/31-01-25/Task-15-19247.py
@@ -0,0 +1,9 @@
+def f(x, y, A):
+    return ((x - 3*y) < A) or (y > 400) or (x > 56)
+
+for A in range(0, 1000):
+    if all(f(x, y, A) for x in range(1, 1000) for y in range(1, 1000)):
+        print(A)
+        break
+
+# 54
