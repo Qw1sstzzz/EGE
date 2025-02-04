65b20b1cfa3b359e2c90ac6c67d0c094ba3cfd07
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-04 16:17:21 +0200

    1991😭

---
+++ Classwork/2025_February/04-02-25-Task15/Task-5-14328.py
@@ -0,0 +1,21 @@
+from string import digits, ascii_uppercase
+
+def convert(num, sys):
+    alph = digits + ascii_uppercase
+    r = ''
+    while num:
+        r += alph[num%sys]
+        num //= sys
+    return r[::-1]
+
+ans = []
+for N in range(1, 10_000):
+    s = convert(N, 12)
+    if N % 3 == 0:
+        s = '1' + s + 'B'
+    else:
+        s = '2' + s + '0'
+    R = int(s, 12)
+    if R < 1996:
+        ans.append(R)
+print(max(ans))
