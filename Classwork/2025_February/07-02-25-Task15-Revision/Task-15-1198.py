fb26ea14093c98213d330d32e1cae03969482b48
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-07 16:50:50 +0200

    40😲

---
+++ Classwork/2025_February/07-02-25-Task15-Revision/Task-15-1198.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def f(x):
+    B = 18 <= x <= 52
+    C = 16 <= x <= 41
+    A = A1 <= x <= A2
+    return (B <= A) and ((not C) or A)
+
+line = [x/5 for x in range(15*5, 55*5)]
+ans =[]
+
+for A1, A2 in combinations(line, r=2):
+    if all(f(x) for x in line):
+        ans.append(A2 - A1)
+
+print(max(ans))
