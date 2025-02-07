5baee8f24a47172992b13a6b9e60a894105852c6
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-07 17:20:39 +0200

    16😲

--- Classwork/2025_February/07-02-25-Task15-Revision/Task-15-4278.py
+++ Classwork/2025_February/07-02-25-Task15-Revision/Task-15-4278.py
@@ -1,5 +1,5 @@
 def f(A, x):
-    return (A % 12 == 0) and (530 % x == 0) <= ((A % x != 0) <= (170 % x != 0))
+    return (A % 12 == 0) and ((530 % x == 0) <= ((A % x != 0) <= (170 % x != 0)))
 
 cnt = 0
 for A in range(1, 1001):
