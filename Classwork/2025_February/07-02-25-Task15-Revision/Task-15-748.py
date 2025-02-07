78e575834f4c71947910769b63f5139204398799
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-07 16:49:06 +0200

    13😲

---
+++ Classwork/2025_February/07-02-25-Task15-Revision/Task-15-748.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def f(x):
+    P = 44 <= x <= 49
+    Q = 28 <= x <= 53
+    A = A1 <= x <= A2
+    return (A <= P) or Q
+
+line = [x/5 for x in range(40*5, 55*5)]
+ans =[]
+
+for A1, A2 in combinations(line, r=2):
+    if all(f(x) for x in line):
+        ans.append(A2 - A1)
+
+print(max(ans))
