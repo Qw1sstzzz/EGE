071f8ecbe4dbb8c6f9358e7db1a7ff01aed672fd
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-14 17:29:31 +0200

    58 18612👾

---
+++ Classwork/2025_February/14-02-25-Revision/Task-17-7716.py
@@ -0,0 +1,17 @@
+with open('Task-17-7716-file.txt') as file:
+    data = [int(i) for i in file]
+
+res = []
+maxi = max([i for i in data if ([1 for j in str(i) if int(j) % 2 != 0].count(1) == len(str(i)))])
+print(maxi)
+
+for i in range(len(data) - 1):
+    p1, p2 = data[i], data[i+1]
+    u1 = ([1 for i in str(p1) if int(i) % 2 == 0].count(1) == len(str(p1)))
+    u2 = ([1 for i in str(p2) if int(i) % 2 == 0].count(1) == len(str(p1)))
+    if (u1 + u2) >= 1:
+        if (p1 + p2) > maxi:
+            res.append(p1 + p2)
+print(len(res), max(res))
+
+
