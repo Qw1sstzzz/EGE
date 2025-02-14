5a02715c6c38b3e3b9e9568a5f557debc77ceaad
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-14 16:21:48 +0200

    166👾

---
+++ Classwork/2025_February/14-02-25-Revision/Task-5-9736.py
@@ -0,0 +1,11 @@
+res = []
+for N in range(1, 10_000):
+    s = bin(N)[2:]
+    if N % 3 == 0:
+        s += s[-3:]
+    else:
+        s += bin(N % 3 * 3)[2:]
+    R = int(s, 2)
+    if R <= 170:
+        res.append(R)
+print(max(res))
