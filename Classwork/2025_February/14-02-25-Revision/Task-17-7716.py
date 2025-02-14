c65c897829569a528303b1e4658cc05f52df4ad0
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-14 17:46:30 +0200

    56 18612👾

--- Classwork/2025_February/14-02-25-Revision/Task-17-7716.py
+++ Classwork/2025_February/14-02-25-Revision/Task-17-7716.py
@@ -8,7 +8,7 @@ print(maxi)
 for i in range(len(data) - 1):
     p1, p2 = data[i], data[i+1]
     u1 = ([1 for i in str(p1) if int(i) % 2 == 0].count(1) == len(str(p1)))
-    u2 = ([1 for i in str(p2) if int(i) % 2 == 0].count(1) == len(str(p1)))
+    u2 = ([1 for i in str(p2) if int(i) % 2 == 0].count(1) == len(str(p2)))
     if (u1 + u2) >= 1:
         if (p1 + p2) > maxi:
             res.append(p1 + p2)
