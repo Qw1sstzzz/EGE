222c86f07fa4bd34a64c78a8f913b1fbbe5ce1a5
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-02-16 15:51:08 +0200

    wxyz🥶

--- Homework/2025_February/16-02-25-Variant/Task-2.py
+++ Homework/2025_February/16-02-25-Variant/Task-2.py
@@ -9,6 +9,7 @@ for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
         (a4, 0, a5, 0),
         (0, a6, 1, 0)
     ]
+
     if len(set(table)) == len(table):
         for p in permutations('xyzw'):
             u = [f(**dict(zip(p, t))) for t in table]
