8fe08ac1c2f2b200cfe5662649e362e808f8ab27
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-02-16 16:12:51 +0200

    56🥶

---
+++ Homework/2025_February/16-02-25-Variant/Task-6.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+lt(90)
+m = 150
+screensize(10000, 10000)
+tracer(0)
+
+up()
+for i in range(10):
+    rt(120)
+    fd(10*m)
+
+down()
+for i in range(7):
+    fd(15*m)
+    rt(90)
+for i in range(5):
+    rt(60)
+    fd(20*m)
+    rt(30)
+
+up()
+for x in range(-25, 25):
+    for y in range(-25, 25):
+        goto(x*m, y*m)
+        dot(3, 'darkred')
+
+update()
+done()
+print(56, 'or', 57, ')))')
