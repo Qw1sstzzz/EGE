a843d2ffc0d58c5c925af9ffd6867a3599d972a3
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-18 17:43:11 +0200

    315671 630 315677 4410 3135677 13230 3145673 7560 3145679 22680 3165671 3780 3195679 51030 (+2-ой способ с product'ом) 👾😃

--- Classwork/2025_February/18-02-25-Task25/Task-25-14440.py
+++ Classwork/2025_February/18-02-25-Task25/Task-25-14440.py
@@ -1,4 +1,5 @@
 from fnmatch import fnmatch
+from itertools import product
 
 def prime(num):
     for x in range(2, int(num * 0.5) + 1):
@@ -12,6 +13,23 @@ def multiply(num):
         r *= int(i)
     return r
 
+'''
 for x in range(315670, 10**7):
     if fnmatch(str(x), '31*567?') and prime(x):
         print(x, multiply(x))
+'''
+
+res = []
+
+for r in range(2):
+    for z in product('0123456789', repeat=r):
+        z = ''.join(z)
+        for v in '0123456789':
+            num = int(f'31{z}567{v}')
+            if prime(num):
+                res.append([num, multiply(num)])
+
+res = sorted(res)
+
+for i in res:
+    print(*i)
