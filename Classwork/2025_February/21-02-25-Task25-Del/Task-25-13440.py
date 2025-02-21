80f8bff19ee3913148ce12ad8e74d48c44f5602f
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-21 16:25:17 +0200

    85316484 32098 850169274 319853 851166024 320228 852162774 320603 854169564 321358 855166314 321733 856163064 322108 858169854 322863 859166604 323238 👾😃

--- Classwork/2025_February/21-02-25-Task25-Del/Task-25-13440.py
+++ Classwork/2025_February/21-02-25-Task25-Del/Task-25-13440.py
@@ -1,5 +1,5 @@
 from fnmatch import fnmatch
 
-for x in range(2658, 10**9 + 1, 2658):
-    if fnmatch(str(x), '85*16?4'):
+for x in range(2658, 10**9, 2658):
+    if fnmatch(str(x), '85?16*4'):
         print(x, x//2658)
