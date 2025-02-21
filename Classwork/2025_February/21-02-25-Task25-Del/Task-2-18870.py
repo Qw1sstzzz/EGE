0558622dcc0ce4664c9ac8e72ca6cc89cc7d3af0
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-21 16:16:51 +0200

    yzxw👾😃

---
+++ Classwork/2025_February/21-02-25-Task25-Del/Task-2-18870.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f(x, z, w, y):
+    return ((x <= z) <= w) or (not y)
+
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [
+        (a1, a2, 1, a3),
+        (a4, 0, a5, a6),
+        (a7, 1, 0, 0)
+    ]
+
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [0, 0, 0]:
+                print(*p)
+# yzxw
