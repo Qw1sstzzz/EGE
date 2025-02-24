35043d05d0e96e6b4b7ee5a5a7510deb37d58bf5
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-02-25 00:13:15 +0300

    608812143 65953 608812143 65953 2086012149 225979 4440212541 481011 6286412541 681011 8486012145 919295 (Только через itertools) 😨

---
+++ Homework/2025_February/25-02-25-Task25/Task-25-20288.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+ans = []
+for r in range(6):
+    for z in product('02468', repeat=r):
+        z = ''.join(z)
+        for v1 in '13579':
+            for v2 in '13579':
+                x = int(f'{z}12{v1}4{v2}')
+                if x > 10**10:
+                    quit()
+                if x % 9231 == 0:
+                    ans.append([x, x//9231])
+
+ans = sorted(ans)
+
+for i in ans:
+    print(*i)
