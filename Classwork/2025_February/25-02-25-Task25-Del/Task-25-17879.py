0a70894936572574bbcd8c223daade17b4b8a61c
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-25 17:55:08 +0200

    800004 400004 800009 114294 800013 266674 800024 400014 800033 61554 😘

---
+++ Classwork/2025_February/25-02-25-Task25-Del/Task-25-17879.py
@@ -0,0 +1,22 @@
+def dell(num):
+    r = set()
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            r |= {i, num//i}
+    return sorted(r)
+
+def M(arr):
+    if arr:
+        return max(arr) + min(arr)
+    return 0
+
+cnt = 0
+for x in range(800_001, 10_000_000):
+    divs = dell(x)
+    m = M(divs)
+
+    if m % 10 == 4:
+        print(x, m)
+        cnt += 1
+    if cnt == 5:
+        break
