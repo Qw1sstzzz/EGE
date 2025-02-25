e37d01d05d8c7dc4daf249520f45c6a64a7da9da
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-25 16:45:10 +0200

    8136574079 152969 8136680461 152971 8136786843 152973 8136893225 152975 8136999607 152977 😘

--- Homework/2025_February/25-02-25-Task25/Task-25-7357.py
+++ Homework/2025_February/25-02-25-Task25/Task-25-7357.py
@@ -2,12 +2,13 @@ from itertools import *
 
 ans = []
 for r in range(7):
-    for z in product('13579', repeat=r):
+    for z in product('0123456789', repeat=r):
         z = ''.join(z)
-        for v in '2468':
-            x = int(f'{v}136{z}')
-            if x % 53191 == 0:
-                ans.append([x, x//53191])
+        if z and int(z) % 2 != 0:
+            for v in '2468':
+                x = int(f'{v}136{z}')
+                if x % 53191 == 0:
+                    ans.append([x, x//53191])
 
 ans = sorted(ans)
 
