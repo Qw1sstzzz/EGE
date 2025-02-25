7001dd8585863fb3b689b37d8414b0873c4e58b7
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-02-25 16:33:42 +0200

    12340001 123400010 123437000 1234000100 1234370000 😘

---
+++ Classwork/2025_February/25-02-25-Task25-Del/Task-25-8379.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def qub(num):
+    return sum(map(int, num))**3
+
+
+ans = []
+for r in range(7):
+    for z in product('0123456789', repeat=r):
+        z = ''.join(z)
+        x = int('1234' + z)
+        if (x % 137 == 0) and (int(z) % qub(z) == 0):
+            ans.append(x)
+
+ans = sorted(ans)
+
+for i in ans:
+    print(i)
