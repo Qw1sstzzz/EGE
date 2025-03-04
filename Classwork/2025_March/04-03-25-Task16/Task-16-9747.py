a049bad3979fce09ab4c5770434192229258e605
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-04 17:03:37 +0200

    6069😤

---
+++ Classwork/2025_March/04-03-25-Task16/Task-16-9747.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+@lru_cache(None)
+
+def f(n):
+    if n < 11: return n
+    if n >= 11: return n + f(n-1)
+
+for i in range(11, 2024):
+    f(i)
+
+print(f(2024) - f(2021))
