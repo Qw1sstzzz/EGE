17634774bbe65ac4f8ab3c0348892aec7eb92cc5
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-06 20:59:05 +0300

    8094😨

---
+++ Homework/2025_March/07-03-25-Task16/Task-16-20196.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+@lru_cache(None)
+
+def f(n):
+    if n < 110: return n
+    if n >= 110: return n + f(n-1)
+
+for i in range(111, 2026):
+    f(i)
+
+print(f(2025) - f(2021))
