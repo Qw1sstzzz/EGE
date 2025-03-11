b05c78020a795de6dcfd8ae8d200b9a59c1a009c
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-11 16:10:23 +0200

    94😤

---
+++ Classwork/2025_March/11-03-25-Task23/Task-5-7607.py
@@ -0,0 +1,9 @@
+ans = []
+for N in range(1, 10_000):
+    s = bin(N)[2:]
+    if N % 3 == 0: s += s[-3:]
+    else: s += bin(N % 3 * 3)[2:]
+    R = int(s, 2)
+    if R > 76:
+        ans.append([N, R])
+print(min(ans))
