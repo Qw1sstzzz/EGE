d0623376b7c621376310fd3f5186f07f13cb41bc
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-15 23:04:17 +0200

    129 (Сломанная задача, которая почему-то не решается нормально регулярками)👨‍💻

--- Saturday(24-27)/ex. №24/Task-24-18937.py
+++ Saturday(24-27)/ex. №24/Task-24-18937.py
@@ -1,12 +1,25 @@
-from re import *
-
 with open('Task-24-18937-file.txt') as file:
-    data = file.readline()
+    data = file.readline().strip()
 
-num = r'[1-9][0-9]*'
+m = 0
 
-reg = rf'{num}([+*]{num})+'
+data = data.replace('*', '+')
+while '++' in data: data = data.replace('++', '+ +')
+for c in '345': data = data.replace(c, '2')
 
-m = max([x.group() for x in finditer(reg, data)], key=len)
+for n in range(1, 20):
+    data = data.replace(f' {'0'*n}2', ' 2')
+for n in range(1, 20):
+    data = data.replace(f'+{'0'*n}2', '+0 2')
+for n in range(2, 20):
+    data = data.replace(f'+{'0'*n}+', '+0 0+')
 
-print(len(m))
+data = data.split()
+for i in data:
+    i = i.strip('+')
+    if len(i) > m and i.count('+') > 0:
+        m = max(m, len(i))
+
+print(m)
+
+
