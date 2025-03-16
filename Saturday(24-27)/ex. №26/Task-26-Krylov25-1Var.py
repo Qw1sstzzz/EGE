225fe8dcce81fbe2de81de6e41da397765a8d6f8
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-16 14:01:22 +0200

    1952 2364 (Сложная задача про морской бой / конференц. залы)👨‍💻

--- Saturday(24-27)/ex. №26/Task-26-Krylov25-1Var.py
+++ Saturday(24-27)/ex. №26/Task-26-Krylov25-1Var.py
@@ -1,4 +1,19 @@
-with open('Task-26-Krylov25-1Var-file.txt.txt') as file:
+with open('Task-26-Krylov25-1Var-file.txt') as file:
     N, M, K = map(int, file.readline().split())
     ships = [list(map(int, i.split())) for i in file]
 
+ships = sorted(ships, key=lambda x: (x[1], -x[0]))
+ans = []
+
+pole = [M + 1] * (K + 1)
+for hor, vert in ships:
+    pole[vert] = hor
+
+# i = столбец (размещение по горизонтали)
+for i in range(1, len(pole) - 1):
+    p1, p2 = pole[i:i+2]
+    ans.append([min(p1, p2) - 1, i])
+
+ans = sorted(ans, key=lambda x: (-x[0], x[1]))
+
+print(ans[0])
