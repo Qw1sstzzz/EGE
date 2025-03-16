2cdb1a6d40b6f0a30ab8c2d16d9cc363ae80f348
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-16 14:21:45 +0200

    9991 5642 (Сложная задача про конференц. зал, с основной волны 2024)👨‍💻

--- Saturday(24-27)/ex. №26/Task-26-17537.py
+++ Saturday(24-27)/ex. №26/Task-26-17537.py
@@ -2,9 +2,17 @@ with open('Task-26-17537-file.txt') as file:
     N, M, K = map(int, file.readline().split())
     places = [list(map(int, i.split())) for i in file]
 
-places = sorted(places, key=lambda x: (x[1], -x[0]))
-hall = [M + 1] * (K + 1)
-for h, v in places:
-    hall[v] == h
+reserved = [M + 1] * (K + 1)
+places = sorted(places, key= lambda x: (x[1], -x[0]))
+ans = []
 
-print(places[:10])
+for h, v in places:
+    reserved[v] = h
+
+for i in range(1, len(reserved) - 1):
+    p1, p2 = reserved[i], reserved[i + 1]
+    ans.append([min(p1, p2) - 1, i])
+
+ans = sorted(ans, key=lambda x: (-x[0], -x[1]))
+
+print(ans[0])
