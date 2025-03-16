300e778b1df04a3926e6c468486fc7087ccd9fc2
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-16 16:42:16 +0200

    851 (Пределы от -50 до 50, работа с множествами, сложное задание)👨‍💻

---
+++ Homework/2025_March/16-03-25-Task23-bolno/Task-23-3374.py
@@ -0,0 +1,11 @@
+def f(a, b, nums=frozenset()):
+    if a == b: return 1
+
+    if a in nums or abs(a) > 50:
+        return 0
+    res = 0
+    for s in (a + 2, a - 3):
+        res += f(s, b, nums | {a})
+    return res
+
+print(f(1, 30))
