566bb2461769ec935c39665ee9bcf3e097de5ed4
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-18 16:41:44 +0200

    72🐍

---
+++ Classwork/2025_March/18-03-25-Revision/Task-6-17625.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+lt(90)
+m = 20
+tracer(0)
+screensize(2500, 2500)
+
+for i in range(10):
+    fd(22*m)
+    rt(90)
+    fd(16*m)
+    rt(90)
+up()
+fd(1*m)
+rt(90)
+fd(1*m)
+lt(90)
+down()
+
+for i in range(10):
+    fd(72*m)
+    rt(90)
+    fd(79*m)
+    rt(90)
+up()
+
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x*m, y*m)
+        dot(5, 'white')
+
+update()
+done()
