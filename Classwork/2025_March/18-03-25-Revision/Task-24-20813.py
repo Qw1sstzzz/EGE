3b010dd237c850482ddd5b856f1c862829001373
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-18 17:59:39 +0200

    174 | 103 🐍

--- Classwork/2025_March/18-03-25-Revision/Task-24-20813.py
+++ Classwork/2025_March/18-03-25-Revision/Task-24-20813.py
@@ -2,7 +2,7 @@ from re import *
 
 with open('Task-24-20813-file.txt') as file:
     data = file.readline().strip()
-
+'''
 num = r'[7-9][0-9]*'
 
 reg = rf'{num}([-*]{num})+'
@@ -10,3 +10,26 @@ reg = rf'{num}([-*]{num})+'
 m = max([x.group() for x in finditer(reg, data)], key=len)
 
 print(len(m), m)
+'''
+
+m = 0
+data = data.replace('-', '*')
+while '**' in data: data = data.replace('**', '* *')
+for c in '89':
+    data = data.replace(c, '7')
+for c in range(2, 50):
+    data = data.replace(f' {'0' * c}', ' 0')
+for v in range(2, 50):
+    data = data.replace(f' {'0' * c} ', ' 0 ')
+for v in range(2, 50):
+    data = data.replace(f'*{'0' * c}', '*0 0')
+for v in range(2, 50):
+    data = data.replace(f'*{'0' * c}*', '*0 0*')
+
+data = data.split()
+for i in data:
+    if len(i) > m:
+        if i.count('*') >= 1:
+            m = max(len(i), m)
+print(m)
+
