e1081fc38cab09a0e085c333312af02dd95b835f
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-18 16:21:54 +0200

    38🐍

---
+++ Classwork/2025_March/18-03-25-Revision/Task-1-17514.py
@@ -0,0 +1,10 @@
+from itertools import *
+
+graph = 'AB BH AH HF FD FG GC CE EG AE DC'.split()
+matrix = '247 148 578 126 38 47 136 235'.split()
+
+print(*range(1, 9))
+
+for i in permutations('ABCDEFGH'):
+    if all(str(i. index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
