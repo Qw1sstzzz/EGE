3263cf973af71dc1cfb3bf90913f5663a1592d3a
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-25 16:39:48 +0200

    120400 19971

--- Classwork/2025_March/25-03-25-Task19-21(2 kuches)/Task-17-7718.py
+++ Classwork/2025_March/25-03-25-Task19-21(2 kuches)/Task-17-7718.py
@@ -5,10 +5,9 @@ for i in range(len(data) - 1):
     for j in range(i + 1, len(data)):
         p1 = data[i]
         p2 = data[j]
-        if p1 != p2:
-            u1 = ((p1 + p2) % 18 == 0)
-            u2 = ((p1 * p2) % 18 == 0)
-            if u1 + u2 == 1:
-                ans.append(p1 + p2)
+        u1 = ((p1 + p2) % 18 == 0)
+        u2 = ((p1 * p2) % 18 == 0)
+        if (u1 + u2) == 1:
+            ans.append(p1 + p2)
 print(len(ans), max(ans))
 
