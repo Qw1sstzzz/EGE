90938e467db023768a84ff9c1f385b5963d86b4e
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-26 13:48:43 +0200

    9😧

--- Variants/Probnik_26-03-25/Task-13.py
+++ Variants/Probnik_26-03-25/Task-13.py
@@ -1,10 +1,9 @@
 from ipaddress import *
-
+ip = ip_address('192.168.24.64')
 for m in range(33):
     net = ip_network(f'192.168.24.112/{m}', 0)
-    ip = ip_address('192.168.24.64')
     if ip in net:
         mask = f'{net.netmask:b}'
-        print(m, mask)
+        print(m, mask[16:].count('1'))
 
 # 9 ?
