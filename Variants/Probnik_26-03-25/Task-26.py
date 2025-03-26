143cce2760f9901f758d82d4a2824eae20a08c6c
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-26 13:21:40 +0200

    62 13150😧

---
+++ Variants/Probnik_26-03-25/Task-26.py
@@ -0,0 +1,24 @@
+with open('Task-26-file.txt') as file:
+    N = int(file.readline())
+    data = []
+    for i in file:
+        arrive, V = map(int, i.split())
+        if V % 10 != 0:
+            end = arrive + (V // 10 + 1)
+        else:
+            end = arrive + (V // 10)
+        data.append([arrive, V, end])
+
+data = sorted(data, key=lambda x: (x[0], -x[1], x[2]))
+
+approved = [data[0]]
+
+for i in range(N):
+    if approved[-1][-1] <= data[i][0]:
+        approved.append(data[i])
+ans_A = len(approved)
+ans_B = 0
+for i in approved:
+    ans_B += i[1]
+
+print(ans_A, ans_B)
