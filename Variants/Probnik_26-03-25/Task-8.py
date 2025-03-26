8d69407e08283b9ddb85ed7b2e3d01a7770d3bad
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-26 11:34:41 +0200

    83635😧

---
+++ Variants/Probnik_26-03-25/Task-8.py
@@ -0,0 +1,12 @@
+from itertools import *
+
+alph = sorted('ЛУНАТИК')
+print(alph)
+ans = []
+for pos, val in enumerate(product(alph, repeat=6), start=1):
+    val = ''.join(val)
+    if val[0] == 'Н':
+        gl_cnt = len([i for i in val if i in 'УАИ'])
+        if gl_cnt == 1:
+            ans.append([pos, val])
+print(max(ans))
