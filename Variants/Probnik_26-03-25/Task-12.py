05faf65c2db887c8135ba7149e00d0ba5e96961d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-03-26 11:56:41 +0200

    8😧

---
+++ Variants/Probnik_26-03-25/Task-12.py
@@ -0,0 +1,11 @@
+
+for n in range(4, 10_000):
+    s = '1' + '2'*n
+    while '12' in s or '32' in s or '22' in s:
+        s = s.replace('12', '22',1)
+        s = s.replace('32', '211',1)
+        s = s.replace('22', '3',1)
+    summ = sum(map(int, s))
+    if summ % 6 == 0:
+        print(n, s, summ)
+        break
