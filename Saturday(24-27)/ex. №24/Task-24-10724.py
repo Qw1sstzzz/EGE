5b464b1b1bb0a973181b80aa51e00104b3d382b4
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-03-29 15:57:26 +0300

    21😨

--- Saturday(24-27)/ex. №24/Task-24-10724.py
+++ Saturday(24-27)/ex. №24/Task-24-10724.py
@@ -4,12 +4,13 @@ with open('Task-24-10724-file.txt') as f:
 
 alph = '0123456789ABCDEF'
 for c in '123456789ABCDEF':
-    data = data.replace(c, '*')
+    data = data.replace(c, '1')
 
 for i in 'GHIJKLMNOPQRSTUVWXYZ':
     data = data.replace(i, ' ')
 
-data = data.replace(' 0', ' ')
+while ' 0' in data:
+    data = data.replace(' 0', ' ')
 data = data.split()
 
 print(len(max(data, key=len)))
