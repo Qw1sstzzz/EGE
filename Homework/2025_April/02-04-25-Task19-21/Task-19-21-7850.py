5d2023206781bae96b8ff0e0e6fa80b6dc4b2269
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-02 23:14:20 +0300

    19) 36 20) 18 33 21) 32 34 😨

---
+++ Homework/2025_April/02-04-25-Task19-21/Task-19-21-7850.py
@@ -0,0 +1,13 @@
+def f(x, m):
+    if x >= 73: return m % 2 == 0
+    if m == 0: return 0
+    h = [
+        f(x+1, m-1),
+        f(x+3, m-1),
+        f(x*2, m-1)
+    ]
+    return any(h) if m % 2 != 0 else all(h)
+
+print('19)', min([s for s in range(1, 73) if f(s, 2)]))
+print('20)', *([s for s in range(1, 73) if not f(s, 1) and f(s, 3)]))
+print('21)', *([s for s in range(1, 73) if not f(s, 2) and f(s, 4)]))
