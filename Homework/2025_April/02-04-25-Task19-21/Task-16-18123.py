69a03c6d00fdcae95fa6f67d4583a2bc038d7060
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-02 23:23:28 +0300

    1😨

---
+++ Homework/2025_April/02-04-25-Task19-21/Task-16-18123.py
@@ -0,0 +1,10 @@
+from functools import *
+@lru_cache(None)
+def f(n):
+    if n >= 2010: return n
+    return f(n+3) + f(n+2) + f(n+1)
+
+for i in range(2000, 2010):
+    f(i)
+
+print((f(2000) - 2*(f(2002) + f(2003))) // f(2004))
