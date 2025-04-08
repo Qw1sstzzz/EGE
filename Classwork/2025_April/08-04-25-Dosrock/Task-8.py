89ea7955d7f34a40629b319e7ed28e0ebad05bc5
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-08 16:26:49 +0200

    1944🔥

---
+++ Classwork/2025_April/08-04-25-Dosrock/Task-8.py
@@ -0,0 +1,12 @@
+from itertools import *
+
+alph = sorted('ДГИАШЭ')
+ans = set()
+
+for i in product(alph, repeat=5):
+    i = ''.join(i)
+    if i[0] not in 'ИАЭ' and i[-1] in 'ИАЭ':
+        ans.add(i)
+print(len(ans))
+
+# 1944
