1981734511f320b1519a663df2f87a4af54d20b0
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-08 17:38:26 +0200

    19🔥

---
+++ Classwork/2025_April/08-04-25-Dosrock/Task-24.py
@@ -0,0 +1,25 @@
+from string import digits, ascii_uppercase
+
+with open('Task-24-file.txt') as file:
+    data = file.readline()
+
+alph = digits + ascii_uppercase
+for c in alph[12:]:
+    data = data.replace(c, ' ')
+
+ans = [[0, '1']]
+data = data.split()
+
+for i in data:
+    if len(i) > max(ans)[0]:
+        i = i.lstrip('0')
+        while len(i) > 2 and int(i, 12) % 2 != 0:
+            i = i[:-1]
+            if int(i, 12) % 2 == 0:
+                ans.append([len(i), i])
+                break
+            else:
+                continue
+print(max(ans))
+
+
