b087cb09eba5a09a62c460feb4415df909d67858
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-11 16:31:58 +0200

    215766🔥

--- Classwork/2025_April/11-04-25-Task13/Task-13-17632.py
+++ Classwork/2025_April/11-04-25-Task13/Task-13-17632.py
@@ -3,7 +3,7 @@ from ipaddress import *
 net = ip_network('112.160.0.0/255.240.0.0', 0)
 cnt = 0
 for ip in net:
-    ip = f'{ip:b}'
+    ip = f'{int(ip):032b}'
     if ip.count('1') % 5 == 0:
         cnt += 1
 print(cnt)
