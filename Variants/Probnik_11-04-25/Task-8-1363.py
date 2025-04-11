39f4b1357475be8ea4c57d53bd6b2e94729332f4
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-11 16:02:05 +0200

    Неправильно🔥

--- Variants/Probnik_11-04-25/Task-8-1363.py
+++ Variants/Probnik_11-04-25/Task-8-1363.py
@@ -1,11 +1,11 @@
 from itertools import *
 
 alph = '01234'
-cnt = 0
+cnt = set()
 
 for val in product(alph, repeat=6):
     s = ''.join(val)
-    if s[0] != '0' and s[0] == '3':
+    if s[0] == '3':
         if s[-1] in '024':
-            cnt += 1
-print(cnt)
+            cnt.add(s)
+print(len(cnt))
