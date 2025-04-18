e81f8ec1553b6a1af704ea2a04da618033f0640d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-18 15:17:22 +0200

    78😩

--- Variants/Probnik_11-04-25/Task-24-19149.py
+++ Variants/Probnik_11-04-25/Task-24-19149.py
@@ -2,8 +2,12 @@ from re import *
 with open('Task-24-19149-file.txt') as file:
     data = file.readline()
 
-num = r'[1-4]*[24]'
+num = r'[1-4][1-4]*'
 reg = rf'[(]{num}([+]{num})+[)]'
-
+m = 0
 x = [i.group() for i in finditer(reg, data)]
-print(len(max(x, key=len)), max(x, key=len))
+for i in x:
+    if eval(i) % 2 == 0:
+        m = max(len(i), m)
+
+print(m)
