9a6c17a5b401180d74353fefb3dae5bf675e6a58
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-18 15:32:42 +0200

    120😩

--- Classwork/2025_April/15-04-25-Revision/Task-13-12451.py
+++ Classwork/2025_April/15-04-25-Revision/Task-13-12451.py
@@ -2,11 +2,13 @@ from ipaddress import *
 
 cnt = 0
 for A in range(0, 256):
+    ip = ip_address(f'246.81.65.{A}')
     net = ip_network(f'246.81.65.{A}/255.255.255.224', 0)
-    for ip in list(net)[1:-1]:
-        ip = f'{int(ip):032b}'
-        if not (ip[16:24].count('0') > ip[24:].count('0')):
-            break
-    else:
-        cnt += 1
+    if ip != net.network_address and ip != net.broadcast_address:
+        for i in list(net)[1:-1]:
+            i = f'{int(i):032b}'
+            if not (i[16:24].count('0') > i[24:].count('0')):
+                break
+        else:
+            cnt += 1
 print(cnt)
