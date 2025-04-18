00e178e977d28686f0517952a51606b334711380
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-18 16:25:08 +0200

    Не дорешали😩

--- Saturday(24-27)/ex. №24/Task-24-21597.py
+++ Saturday(24-27)/ex. №24/Task-24-21597.py
@@ -3,10 +3,12 @@ from re import *
 with open('Task-24-21597-file.txt') as f:
     data = f.readline()
 
-num = r'([1-5][1-5]*)'
+num = r'([1-5][0-5]*)'
+m = 0
+reg = rf'({num}[*])+({num}[-])+{num}'
+r = rf'(?=({reg}))'
+match = [x.group(1) for x in finditer(r,data)]
 
-reg = rf'[({num}\*)]+'
-match = [x.group() for x in finditer(reg,data)]
 for i in match:
-    print(i)
-    input()
+    m = max(len(i), m)
+print(m)
