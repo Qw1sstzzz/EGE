80fac7cb5ec6ea1d85c1e54dc5975e0cea92cc03
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-21 19:49:59 +0200

    32

---
+++ Variants/Probnik_21-04-25/Task-5-6588.py
@@ -0,0 +1,11 @@
+ans = []
+for N in range(1, 10_000):
+    s = bin(N)[2:]
+    s = s.replace('0', '*').replace('1', '0').replace('*', '1')
+    s = '1' + s
+    if s[-1] == '0': s += '0'
+    else: s += '1'
+    R = int(s, 2)
+    if R > 180:
+        ans.append([N, R])
+print(min(ans))
