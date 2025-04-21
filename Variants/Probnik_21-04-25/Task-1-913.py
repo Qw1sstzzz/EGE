fee677b42a02c7ff075f549c85f8f27e6c3fdf79
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-21 19:07:02 +0200

    34

--- Variants/Probnik_21-04-25/Task-1-913.py
+++ Variants/Probnik_21-04-25/Task-1-913.py
@@ -1,10 +1,10 @@
 from itertools import *
 
-graph = 'KR KP RT TP TL TC RC PL CL LM MC'.split()
-matrix = '235 14 1456 236 1367 3457 56'.split()
+graph = 'AE AC AB BD BF DF DC CE EG GF'.split()
+matrix = '235 146 145 236 137 247 56'.split()
 
 print(*range(1, 8))
 
-for i in permutations('KPRTLCM'):
+for i in permutations('ABCDEFG'):
     if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
         print(*i)
