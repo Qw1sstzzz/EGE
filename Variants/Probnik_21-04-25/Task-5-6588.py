23625df19ee1740615df3c0343dbd2e3338b997c
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-21 20:31:55 +0200

    32

--- Variants/Probnik_21-04-25/Task-5-6588.py
+++ Variants/Probnik_21-04-25/Task-5-6588.py
@@ -3,7 +3,7 @@ for N in range(1, 10_000):
     s = bin(N)[2:]
     s = s.replace('0', '*').replace('1', '0').replace('*', '1')
     s = '1' + s
-    if s[-1] == '0': s += '0'
+    if s.count('1') % 2 == '0': s += '0'
     else: s += '1'
     R = int(s, 2)
     if R > 180:
