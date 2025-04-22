dc7c7a1980179e075f61c64b2b6b14ef53990f1d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-22 17:26:14 +0200

    21😠

---
+++ Classwork/2025_April/22-04-25-Revision/Task-13-10782.py
@@ -0,0 +1,12 @@
+from ipaddress import *
+
+ip1 = ip_address('118.187.59.255')
+ip2 = ip_address('118.187.65.115')
+
+for m in range(17, 33):
+    net1 = ip_network(f'{ip1}/{m}', False)
+    net2 = ip_network(f'{ip2}/{m}', False)
+    if net1 != net2:
+        if ip1 not in (net1.broadcast_address, net1.network_address):
+            if ip2 not in (net2.broadcast_address, net2.network_address):
+                print(m)
