c5bf4ad81b3a544f5a591f1869f507adb99a99f8
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-22 17:32:19 +0200

    23😠

---
+++ Classwork/2025_April/22-04-25-Revision/Task-15-2861.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def f(x):
+    P = 69 <= x <= 91
+    Q = 77 <= x <= 114
+    A = A1 <= x <= A2
+    return Q <= ((P == Q) or ((not P) <= A))
+
+line = [x/5 for x in range(68*5, 115*5)]
+ans = []
+
+for A1, A2 in combinations(line, r=2):
+    if all(f(x) for x in line):
+        ans.append(A2 - A1)
+print(min(ans))
