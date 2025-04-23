ca0f4d066a318e05dcbe577c81233cca35e87bbb
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-23 18:13:31 +0200

    38306

---
+++ Variants/Probnik_23-04-25(EGKR)/Task-8-21703.py
@@ -0,0 +1,11 @@
+from itertools import *
+
+alph = sorted('ПОБЕДА')
+ans = []
+
+for pos, val in enumerate(product(alph, repeat=6), start=1):
+    s = ''.join(val)
+    if pos % 2 == 0:
+        if s[0] == 'О' and len(set(s)) == len(s):
+            ans.append([pos, s])
+print(max(ans))
