ab8f48ea1bbd6d2abba6b43d7dbb6d120b298a91
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-04-23 18:33:50 +0200

    1591

---
+++ Variants/Probnik_23-04-25(EGKR)/Task-12-21707.py
@@ -0,0 +1,10 @@
+ans = []
+for n in range(4, 10_000):
+    s = '4' + '2' * n
+    while '42' in s or '8222' in s or '2222' in s:
+        s = s.replace('42', '2', 1)
+        s = s.replace('8222', '24', 1)
+        s = s.replace('2222', '8', 1)
+    if sum(map(int, s)) == 110:
+        ans.append(n)
+print(min(ans))
