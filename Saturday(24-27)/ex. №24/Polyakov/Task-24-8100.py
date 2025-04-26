5e00ad3331490e3bb3b174c689f9023c5796082f
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-26 16:45:32 +0200

    55😠

--- Saturday(24-27)/ex. №24/Polyakov/Task-24-8100.py
+++ Saturday(24-27)/ex. №24/Polyakov/Task-24-8100.py
@@ -3,7 +3,7 @@ from re import *
 with open('Task-24-335-file.txt') as f:
     data = f.readline()
 
-num1 = r'[1-9][0-9]*[12346789]|[1-9]'
+num1 = r'[1-9][0-9]*[12346789]|[12346789]'
 num2 = r'[1-9][0-9]*[05]|[05]'
 
 express = rf'\(({num1})[-+]({num2})\)'
