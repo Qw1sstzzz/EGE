8588937731162d3f855fcd88e5a4bdf88cda79cf
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-04-29 17:15:35 +0200

    10738 30730 37522 51277 😠

---
+++ Classwork/2025_April/29-04-25-Task27/Task-27-17882.py
@@ -0,0 +1,43 @@
+clustersA = [[], []]
+clustersB = [[], [], []]
+
+with open('Task-27A-17882-file.txt') as f:
+    for line in f:
+        x, y = map(float, line.split())
+        if x < 1:
+            clustersA[0].append([x, y])
+        else:
+            clustersA[1].append([x, y])
+
+with open('Task-27B-17882-file.txt') as f:
+    for line in f:
+        x, y = map(float, line.split())
+        if y < 3:
+            clustersB[0].append([x, y])
+        elif y < 7:
+            clustersB[1].append([x, y])
+        else:
+            clustersB[2].append([x, y])
+
+def d(A, B):
+    x1, y1 = A
+    x2, y2 = B
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+def center(cl):
+    m = []
+    for c in cl:
+        sm = sum(d(c, c1) for c1 in cl)
+        m.append([sm, c])
+    return min(m)[1]
+
+centersA = [center(cl) for cl in clustersA]
+centersB = [center(cl) for cl in clustersB]
+
+PxA = sum(x for x, y in centersA) * 10_000 / 2
+PxB = sum(x for x, y in centersB) * 10_000 / 3
+PyA = sum(y for x, y in centersA) * 10_000 / 2
+PyB = sum(y for x, y in centersB) * 10_000 / 3
+
+print(int(PxA), int(PyA))
+print(int(PxB), int(PyB))
