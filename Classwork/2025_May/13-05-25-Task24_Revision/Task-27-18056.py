42dd72007ceb87e0df8d6cc618558a341d214454
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-13 16:35:30 +0200

    43744 47901 99895 100091 😠

---
+++ Classwork/2025_May/13-05-25-Task24_Revision/Task-27-18056.py
@@ -0,0 +1,54 @@
+def d(A, B):
+    x1, y1 = A
+    x2, y2 = B
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** .5
+
+def center(cl):
+    m = []
+    for c in cl:
+        summ = sum([d(c, c1) for c1 in cl])
+        m.append([summ, c])
+    return min(m)[1]
+
+with open('Task-27A-18056-file.txt') as f:
+    dataA = [list(map(float, i.split())) for i in f]
+
+clustersA = []
+eps = 0.8
+
+while dataA:
+    cluster = [dataA.pop()]
+    for dot in cluster:
+        for dot2 in dataA.copy():
+            if d(dot, dot2) < eps:
+                cluster.append(dot2)
+                dataA.remove(dot2)
+    if len(cluster) > 10:
+        clustersA.append(cluster)
+
+with open('Task-27B-18056-file.txt') as f:
+    dataB = [list(map(float, i.split())) for i in f]
+
+clustersB = []
+eps = 0.4
+
+while dataB:
+    cluster = [dataB.pop()]
+    for dot in cluster:
+        for dot2 in dataB.copy():
+            if d(dot, dot2) < eps:
+                cluster.append(dot2)
+                dataB.remove(dot2)
+    if len(cluster) > 10:
+        clustersB.append(cluster)
+
+centersA = [center(cl) for cl in clustersA]
+centersB = [center(cl) for cl in clustersB]
+
+PxA = sum([x for x, y in centersA]) / 2 * 100_000
+PyA = sum([y for x, y in centersA]) / 2 * 100_000
+PxB = sum([x for x, y in centersB]) / 3 * 100_000
+PyB = sum([y for x, y in centersB]) / 3 * 100_000
+
+print(abs(int(PxA)), abs(int(PyA)))
+print(abs(int(PxB)), abs(int(PyB)))
