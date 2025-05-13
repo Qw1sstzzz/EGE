13b902165854c4082c5a96178a3a37815ceaafc6
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-13 16:58:42 +0200

    19😠

--- Classwork/2025_May/13-05-25-Task24_Revision/Task-24-3228.py
+++ Classwork/2025_May/13-05-25-Task24_Revision/Task-24-3228.py
@@ -2,7 +2,6 @@ with open('Task-24-3228-file.txt') as f:
     data = f.readline()
 
 data = data.replace('AB', '*').replace('AC', '*')
-
 data = data.replace('A', ' ').replace('B', ' ').replace('C', ' ')
 
 print(len(max(data.split(), key=len)))
