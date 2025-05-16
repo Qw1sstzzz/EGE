9dab2c1266795e28105a83f46acc30c2e38033d8
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-16 16:18:56 +0200

    1023😠

--- Classwork/2025_May/16-05-25-Task24_Revision/Task-5-4869.py
+++ Classwork/2025_May/16-05-25-Task24_Revision/Task-5-4869.py
@@ -1,8 +1,8 @@
 for N in range(2, 10_000):
     s = bin(N)[2:]
-    cnt_0_on_chet_pos = len([s[i] for i in range(len(s)) if s[i] == '0' and i % 2 != 0])
-    cnt_1_on_nechet_pos = len([s[i] for i in range(len(s)) if s[i] == '1' and i % 2 == 0])
-    R = abs(cnt_1_on_nechet_pos - cnt_0_on_chet_pos)
+    cnt_0_on_nechet_pos = len([s[i] for i in range(len(s)) if s[i] == '0' and i % 2 == 0])
+    cnt_1_on_chet_pos = len([s[i] for i in range(len(s)) if s[i] == '1' and i % 2 != 0])
+    R = abs(cnt_1_on_chet_pos - cnt_0_on_nechet_pos)
     if R == 5:
         print(N)
         break
