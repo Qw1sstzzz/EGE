d2342f7b13069f1fbf83cd4904f4aade6a074b0d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-20 16:32:39 +0200

    1😠

---
+++ Classwork/2025_May/20-05-25-Task24_Revision_x3/Task-15-6827.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def f(x):
+    P = 257 <= x <= 1000
+    Q = 5 <= x <= 100
+    R = 99 <= x <= 258
+    A = A1 <= x <= A2
+    return (A <= R) and ((not (A <= P)) <= Q)
+
+line = [x/5 for x in range(5*5, 300*5)]
+ans = []
+
+for A1, A2 in combinations(line, r=2):
+    if all(f(x) for x in line):
+        ans.append(A2- A1)
+
+print(min(ans))
