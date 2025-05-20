79155349a9fdff682c12e56dd807de6f19cc4957
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-20 16:16:30 +0200

    889 283😠

---
+++ Classwork/2025_May/20-05-25-Task24_Revision_x3/Task-17-5882.py
@@ -0,0 +1,20 @@
+with open('Task-17-5882-file.txt') as f:
+    data = [int(i) for i in f]
+
+ans = []
+
+minis = []
+for i in range(len(data) - 1):
+    p1, p2 = data[i], data[i+1]
+    u1 = str(p1)[-1] == '1'
+    u2 = str(p2)[-1] == '1'
+    if u1 + u2 == 1:
+        minis.append(min(p1, p2))
+minik = str(min(minis))
+
+for i in data:
+    if str(i).count('3') >= 1:
+        if i >= int(minik[1])**2 + int(minik[2])**2 + int(minik[3])**2 + int(minik[4])**2:
+            ans.append(i)
+
+print(len(ans), min(ans))
