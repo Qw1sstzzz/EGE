19a3cab8ed2c42f6de2b1232872d1ec5a5649a67
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-30 17:08:45 +0200

    59966 50449😠

---
+++ Classwork/2025_May/30-05-25-Task26_Revision/Task-26-4205.py
@@ -0,0 +1,12 @@
+with open("Task-26-4205-file.txt") as f:
+    N = int(f.readline())
+    data = [tuple(map(int, i.split())) for i in f]
+
+data = sorted(data, key=lambda x: (-x[0], x[1]))
+
+for p in zip(data, data[1:]):
+    tree1, tree2 = p
+    if tree2[0] == tree1[0]:
+        if tree2[1] - tree1[1] - 1 == 13:
+            print(*(tree1[0], tree1[1] + 1))
+            break
