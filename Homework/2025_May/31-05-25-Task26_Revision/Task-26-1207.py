7f7808b3132fdab169ea6da460a355188decf739
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-05-31 13:33:25 +0200

    1611 90

---
+++ Homework/2025_May/31-05-25-Task26_Revision/Task-26-1207.py
@@ -0,0 +1,22 @@
+with open('Task-26-1207-file.txt') as f:
+    S, N = map(int, f.readline().split())
+    data = [int(i) for i in f]
+
+data = sorted(data)
+ans = []
+
+for i in data:
+    if S >= i:
+        ans.append(i)
+        S -= i
+    else:
+        break
+
+S += ans.pop(-1)
+
+for j in data[::-1]:
+    if S >= j:
+        ans.append(j)
+        break
+
+print(len(ans), max(ans))
