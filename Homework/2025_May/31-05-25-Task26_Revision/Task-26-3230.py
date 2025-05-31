dd4989f70722247f9400b630e8d3b6c36a8f419e
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-05-31 13:05:09 +0200

    2261 5087

---
+++ Homework/2025_May/31-05-25-Task26_Revision/Task-26-3230.py
@@ -0,0 +1,13 @@
+with open('Task-26-3230-file.txt') as f:
+    N = int(f.readline())
+    data = [list(map(int, i.split())) for i in f]
+
+data = sorted(data, key=lambda x: (-x[0], x[1]))
+ans = []
+
+for i in range(N - 1):
+    tree1, tree2 = data[i], data[i+1]
+    if tree1[0] == tree2[0]:
+        if tree2[1] - tree1[1] - 1 == 11:
+            print(tree2[0], tree1[1] + 1)
+            break
