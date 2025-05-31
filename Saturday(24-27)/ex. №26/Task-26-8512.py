c1be292ee5193b8d6c0b0963254d789fd53d6e1d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-31 17:53:44 +0200

    389 133😠

--- Saturday(24-27)/ex. №26/Task-26-8512.py
+++ Saturday(24-27)/ex. №26/Task-26-8512.py
@@ -1,21 +1,18 @@
-with open('Task-26-8512-file.txt') as file:
-    K = int(file.readline().strip())
-    N = int(file.readline().strip())
-    data = [list(map(int, i.strip().split())) for i in file]
+with open('Task-26-8512-file.txt') as f:
+    K = int(f.readline())
+    N = int(f.readline())
+    data = [list(map(int, i.split())) for i in f]
 
-data = sorted(data, key=lambda x: (x[0], x[1]))
-cells = [0] * K
-last_cell = 0
-cnt = 0
+data = sorted(data)
+ceils = [0]*K
+ans_A, ans_B = 0, 0
 
-for start, end in data:
-    for i in range(len(cells)):
-        if cells[i] < start:
-            cells[i] = end
-            cnt += 1
-            last_cell = i + 1
+for time in data:
+    for i in range(K):
+        if time[0] >= ceils[i]:
+            ceils[i] = time[1] + 1
+            ans_A += 1
+            ans_B = i + 1
             break
 
-print(cnt, last_cell)
-
-# 389 133 🥶
+print(ans_A, ans_B)
