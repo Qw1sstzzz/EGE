a18ad7a0f6041b75d6d5f17786b098a4d9baf4b8
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-05-31 16:09:39 +0200

    177 78😠

---
+++ Saturday(24-27)/ex. №26/Task-26-6056.py
@@ -0,0 +1,12 @@
+with open('Task-26-6056-file.txt') as f:
+    N = int(f.readline())
+    data = [int(i) for i in f]
+
+data = sorted(data)[::-1]
+boxes = [data[0]]
+
+for i in range(1, N):
+    if boxes[-1] - data[i] >= 56:
+        boxes.append(data[i])
+
+print(len(boxes), min(boxes))
