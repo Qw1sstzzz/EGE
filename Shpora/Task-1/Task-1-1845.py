310126222e6df685fbf086a40f3bd4f5047c0a42
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-06-02 23:20:39 +0200

    21

---
+++ Shpora/Task-1/Task-1-1845.py
@@ -0,0 +1,12 @@
+from itertools import *
+
+graph = 'АБ БВ АВ ВД ДГ ГБ ГЕ ЕЗ ЗД'.split()
+matrix = '67 567 456 35 234 123 12'.split()
+
+print(*range(1, 8))
+
+for i in permutations('АБВГДЕЗ'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
+
+print(12 + 9)
