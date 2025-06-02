da87323e79a8149c1baa27a1df07060476c5f85e
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-02 14:33:59 +0200

    wxyz😠

---
+++ Shpora/Task-2/Task-2-9771.py
@@ -0,0 +1,16 @@
+from itertools import *
+
+def f(y, x, z, w):
+    return (y <= x) and (not z) and w
+
+for a1, a2, a3, a4, a5, a6 in product([0, 1], repeat=6):
+    table = [
+        (1, 0, a1, a2),
+        (1, 1, a3, a4),
+        (a5, 1, 1, a6)
+    ]
+    if len(set(table)) == len(table):
+        for p in permutations('xyzw'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [1, 1, 1]:
+                print(*p)
