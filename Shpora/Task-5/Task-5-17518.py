ab6ccdc7e3be0181f35a7297cc79a8dedd5e4498
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-02 15:27:19 +0200

    19😠

---
+++ Shpora/Task-5/Task-5-17518.py
@@ -0,0 +1,12 @@
+ans = []
+
+for N in range(1, 10_000):
+    s = bin(N)[2:]
+    if s.count('1') % 2 == 0:
+        s = '10' + s[2:] + '0'
+    else:
+        s = '11' + s[2:] + '1'
+    R = int(s, 2)
+    if R > 50:
+        ans.append(N)
+print(min(ans))
