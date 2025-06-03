1ea4526a3fb139907212e8ce987062c6c3316e7c
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-03 14:50:43 +0200

    2313😠

--- Shpora/Task-8/Task-8-17549.py
+++ Shpora/Task-8/Task-8-17549.py
@@ -3,7 +3,7 @@ from itertools import *
 alph = sorted('ФОКУС')
 ans = []
 
-for pos, val in enumerate(product(alph, repeat=5)):
+for pos, val in enumerate(product(alph, repeat=5), start=1):
     val = ''.join(val)
     if val.count('Ф') == 0 and val.count('У') == 2:
         ans.append([pos, val])
