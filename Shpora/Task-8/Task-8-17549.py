0dbe097c0cf6d7bfa0000e2020fa321b29d4faf9
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-03 14:49:53 +0200

    2312😠

---
+++ Shpora/Task-8/Task-8-17549.py
@@ -0,0 +1,11 @@
+from itertools import *
+
+alph = sorted('ФОКУС')
+ans = []
+
+for pos, val in enumerate(product(alph, repeat=5)):
+    val = ''.join(val)
+    if val.count('Ф') == 0 and val.count('У') == 2:
+        ans.append([pos, val])
+
+print(max(ans))
