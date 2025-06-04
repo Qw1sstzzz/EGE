1e246e95d186d20f7215a75c81d818f5f5980f7d
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-04 15:05:57 +0200

    9883255254😠

--- Shpora/Task-13/Task-13-21899.py
+++ Shpora/Task-13/Task-13-21899.py
@@ -4,5 +4,5 @@ net = ip_network('98.81.154.195/255.252.0.0', False)
 ip1 = ip_address('98.81.154.195')
 if ip1 not in (net.network_address, net.broadcast_address):
     for ip in list(net)[::-1]:
-        print(ip)
-        input()
+        print(9883255254)
+        break
