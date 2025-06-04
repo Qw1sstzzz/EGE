aa667cbb4d2e9b70257ffd8c0001b26fe88eccf2
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-04 14:32:04 +0200

    33😠

--- Shpora/Task-12/Task-12-9835.py
+++ Shpora/Task-12/Task-12-9835.py
@@ -1,10 +1,9 @@
-ans = []
 for n in range(4, 10_000):
-    s = '1' + '2'*n
-    while '18' in s or '388' in s or '222' in s:
-        s = s.replace('12', '2', 1)
-        s = s.replace('322', '21', 1)
-        s = s.replace('222', '3', 1)
-    ans.append(sum(map(int, s)))
-
-print(max(ans))
+    s = '1' + '8'*n
+    while '18' in s or '388' in s or '888' in s:
+        s = s.replace('18', '8', 1)
+        s = s.replace('388', '81', 1)
+        s = s.replace('888', '3', 1)
+    if s.count('1') == 3:
+        print(n)
+        break
