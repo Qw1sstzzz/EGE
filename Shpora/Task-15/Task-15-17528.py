aad783b5573bfe53470d73d18b0b93ac460b7b78
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-04 15:38:24 +0200

    19😠

---
+++ Shpora/Task-15/Task-15-17528.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def f(x):
+    P = 15 <= x <= 40
+    Q = 21 <= x <= 63
+    A = A1 <= x <= A2
+    return P <= ((Q and (not A)) <= (not P))
+
+line = [x + eps for x in range(15, 63) for eps in (-0.15, 0, 0.15)]
+ans = []
+
+for A1, A2 in combinations(line, r=2):
+    if all(f(x) == 1 for x in line):
+        ans.append(A2-A1)
+
+print(min(ans))
