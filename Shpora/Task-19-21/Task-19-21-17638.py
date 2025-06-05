85e09fe962865f771c174326dc7e5bc4540b7370
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-05 14:37:47 +0200

    19) 19 20) 16 18 21) 15 😠

---
+++ Shpora/Task-19-21/Task-19-21-17638.py
@@ -0,0 +1,13 @@
+def f(x, m):
+    if x >= 39: return m % 2 == 0
+    if m == 0: return 0
+    h = [
+        f(x + 1, m-1),
+        f(x + 3, m-1),
+        f(x * 2, m-1)
+         ]
+    return any(h) if m % 2 != 0 else all(h)
+
+print('19)', min([s for s in range(1, 39) if f(s, 2)]))
+print('20)', *([s for s in range(1, 39) if not f(s, 1) and f(s, 3)]))
+print('21)', min([s for s in range(1, 39) if not f(s, 2) and f(s, 4)]))
