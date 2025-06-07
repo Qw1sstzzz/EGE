5e6131adae5fde034c0bea233e77a178ad816dbe
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-07 15:49:49 +0200

    40031 148😠

--- Shpora/Task-26/Task-26-19256.py
+++ Shpora/Task-26/Task-26-19256.py
@@ -16,17 +16,14 @@ ans = []
 for i in exercises.items():
     ID = i[0]
     Tasks = i[1]
-    cnt = [1]
+    cnt = 0
     c = 1
     for j in range(len(Tasks) - 1):
         if Tasks[j+1] - Tasks[j] == 1:
             c += 1
         else:
-            cnt.append(c)
             c = 1
-            continue
-        cnt.append(c)
-    cnt = max(cnt)
+        cnt = max(c, cnt)
     ans.append([ID, cnt])
 
 ans = sorted(ans, key=lambda x: (-x[1], x[0]))
