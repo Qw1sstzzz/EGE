6885891cbacb019b3986a09789becd7e048ab3f4
GPetrovskiy <clxwnhopelesss@yandex.ru>
2025-06-08 16:12:33 +0200

    21😠

--- Classwork/2025_May/16-05-25-Task24_Revision/Task-24-9791.py
+++ Classwork/2025_May/16-05-25-Task24_Revision/Task-24-9791.py
@@ -1,5 +1,4 @@
 from string import ascii_uppercase
-
 with open('Task-24-9791-file.txt') as f:
     data = f.readline()
 
@@ -7,7 +6,6 @@ for c in ascii_uppercase[6:]:
     data = data.replace(c, ' ')
 
 data = data.split()
-
 ans = 0
 for i in data:
     s = i.lstrip('0')
@@ -19,5 +17,4 @@ for i in data:
                 ps = s[st:end]
                 if int(ps, 16) % 20 == 0:
                     ans = max(ans, len(ps))
-
 print(ans)
