1c2a988c4c0d44372c2f5650f13238f085c52bdf
Qw1sstzzz <clxwnhopelesss@yandex.ru>
2025-06-08 23:47:04 +0200

    832810

---
+++ Variants/18463844_07-06-25/Task-13.py
@@ -0,0 +1,11 @@
+from ipaddress import *
+
+net = ip_network('112.160.0.0/255.240.0.0', False)
+cnt = 0
+
+for ip in net:
+    ip = f'{ip:b}'
+    if ip.count('1') % 5 != 0:
+        cnt += 1
+
+print(cnt)
